c3c27fbcd24f079e801b591463c7f96a6ef8ee3f
anthony luque <kasuru@hotmail.com>
2022-03-27 09:44:47 -0500

    [ADD] swagger basic config

--- src/index.js
+++ src/index.js
@@ -2,17 +2,43 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const RutasProtegidas = require("./utils/RutasProtegidas");
+const path = require("path");
 
 require("dotenv").config();
 require("./database");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API para aprender Exprres-Mongo",
+      description: "Es un proyecto de aprendizaje",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "https://express-mo.herokuapp.com/api/",
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, "./*/view.js")}`],
+};
 
 const PORT = process.env.PORT;
 const userRoutes = require("./users/view");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
-app.use("/user", userRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", RutasProtegidas, function (req, res) {
   console.log(req.decoded);
